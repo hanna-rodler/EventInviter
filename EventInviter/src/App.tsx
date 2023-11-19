b2a86a4939785e547e46d7a9ca8a3284c4f17942
AzureAD\ElenaEbetshuber <elena.ebetshuber@paso-solutions.com>
2023-11-19 22:05:52 +0100

    Update dependencies

--- EventInviter/src/App.tsx
+++ EventInviter/src/App.tsx
@@ -1,35 +1,33 @@
 import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
 import { AppBar, Button, Container, Toolbar } from "@mui/material";
-import { EventsPage } from './pages/EventsPage';
+import { EventsPage } from "./pages/EventsPage";
 import { ContactsPage } from "./pages/ContactsPage";
 import { EventDetailPage } from "./pages/EventDetailPage";
 
-
 function App() {
-
   return (
-      <>
-          <BrowserRouter>
-              <AppBar position="static">
-                  <Toolbar>
-                      <Button color="inherit" component={NavLink} to="/">
-                          Events
-                      </Button>
-                      <Button color="inherit" component={NavLink} to="/contacts">
-                          Contacts
-                      </Button>
-                  </Toolbar>
-              </AppBar>
-              <Container>
-                  <Routes>
-                      <Route path="/" element={<EventsPage />} />
-                      <Route path="/contacts" element={<ContactsPage />} />
-                      <Route path="/events/:id" element={<EventDetailPage />} />
-                  </Routes>
-              </Container>
-          </BrowserRouter>
-      </>
-  )
+    <>
+      <BrowserRouter>
+        <AppBar position="static">
+          <Toolbar>
+            <Button color="inherit" component={NavLink} to="/">
+              Events
+            </Button>
+            <Button color="inherit" component={NavLink} to="/contacts">
+              Contacts
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <Container>
+          <Routes>
+            <Route path="/" element={<EventsPage />} />
+            <Route path="/contacts" element={<ContactsPage contacts={[]} />} />
+            <Route path="/events/:id" element={<EventDetailPage />} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
