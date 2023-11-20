550c1368b5fa28a21769c913f7ddf5d9325c91dc
AzureAD\ElenaEbetshuber <elena.ebetshuber@paso-solutions.com>
2023-11-20 13:56:38 +0100

    Add Provider component to main.tsx

--- EventInviter/src/main.tsx
+++ EventInviter/src/main.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { ThemeProvider } from '@mui/material/styles';
-import customTheme from './customTheme'; // custom theme import
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ThemeProvider } from "@mui/material/styles";
+import customTheme from "./customTheme"; // custom theme import
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
+import "./index.css";
 
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Provider store={store}>
       <ThemeProvider theme={customTheme}>
-          <App />
+        <App />
       </ThemeProvider>
-  </React.StrictMode>,
-)
+    </Provider>
+  </React.StrictMode>
+);
